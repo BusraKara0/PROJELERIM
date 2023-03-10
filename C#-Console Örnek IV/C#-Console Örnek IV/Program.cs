a802303c9322496edf9cbb95fcd48f513b85c043
busrax1 <bkara3406@gmail.com>
2023-03-10 17:02:08 +0300

    Örnek.Console IV
    Project has been created.

---
+++ C#-Console Örnek IV/C#-Console Örnek IV/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Örnek_IV
+{
+	//Değişken olmasını istersek
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Kisiler ks = new Kisiler();
+			string ads;
+			Console.Write("İsim Girin:");
+			ads = Console.ReadLine();
+			ks.kisilistesi(ads);
+			Console.Read();
+		}
+	}
+}

b8c7996215a25f280232cccb5c283a81d1a19301
busrax1 <bkara3406@gmail.com>
2023-03-10 21:01:28 +0300

    Örnek.Form10
    Project has been created.

---
+++ WinFormsApp10/WinFormsApp10/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        String adsoyad, meslek
+                adsoyad = ("Büşra KARA")
+        meslek = ("Öğrenci")
+        Label1.Text = adsoyad
+        Label2.Text = meslek
+    End Sub
+End Class

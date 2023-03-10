f50db07a4d6091489481aea8f40166f90048ac31
busrax1 <bkara3406@gmail.com>
2023-03-10 16:52:42 +0300

    Örnek.Console Ödev II
    Project has been created.

---
+++ C#-Console Ödev II/C#-Console Ödev II/Islem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Ödev_II
+{
+	internal class Islem
+	{ 
+		public int topla(int x, int y)
+		{
+			int z = x + y;
+			Console.WriteLine("Birinci Sayıyı Girin:");
+			Console.WriteLine(x);
+			Console.WriteLine("İkinci Sayıyı Girin:");
+			Console.WriteLine(y);
+			Console.WriteLine("Sonuç:");
+			Console.WriteLine(z);
+			return z;
+		}
+	}
+}
---
+++ C#-Console Ödev II/C#-Console Ödev II/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Ödev_II
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Islem isl = new Islem();
+			isl.topla(4, 5);
+			Console.Read();
+		}
+	}
+}

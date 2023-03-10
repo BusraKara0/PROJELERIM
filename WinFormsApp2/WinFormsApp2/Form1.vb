3b32016cba4d9d484f4c84f7e2a42d3c697924c9
busrax1 <bkara3406@gmail.com>
2023-03-10 20:52:59 +0300

    Örnek.Form2
    Project has been created.

---
+++ WinFormsApp2/WinFormsApp2/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        ComboBox1.Items.Add("Antalya")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        ComboBox1.Items.Add(TextBox1.Text)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Add("Kasiyer")
+        ListBox1.Items.Add("Futbolcu")
+        ListBox1.Items.Add("Muhasebeci")
+        ListBox1.Items.Add("Antrenör")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Add(TextBox2.Text)
+    End Sub
+End Class

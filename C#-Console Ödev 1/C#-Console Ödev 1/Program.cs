e9437d56005dfb69d21d8348ac84f368b75e5272
busrax1 <bkara3406@gmail.com>
2023-03-10 16:50:36 +0300

    Örnek.Console Ödev
    Project has been created.

---
+++ C#-Console Ödev 1/C#-Console Ödev 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Ödev_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Tanıtım tntm = new Tanıtım();
+			string ads;
+			Console.WriteLine("İsim Girin: Büşra");
+			Console.WriteLine("Soyad Girin: KARA");
+			Console.WriteLine("Meslek: Öğrenci");
+			Console.WriteLine("Yaş Giriniz: 19");
+			ads = Console.ReadLine();
+			
+			Console.Read();
+		}
+	}
+}
---
+++ C#-Console Ödev 1/C#-Console Ödev 1/Tanıtım.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Ödev_1
+{
+	internal class Tanıtım
+	{
+		public void tanısma(string ad, string soyad, string meslek, int yas)
+		{
+			Console.WriteLine(ad);
+			Console.WriteLine(soyad);
+			Console.WriteLine(meslek);
+			Console.WriteLine(yas);
+		}
+	}
+}

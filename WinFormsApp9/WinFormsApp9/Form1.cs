e2c551419877c795d82a2826135da6e7047e4dd3
busrax1 <bkara3406@gmail.com>
2023-03-10 21:00:22 +0300

    Örnek.Form9
    Project has been created.

---
+++ WinFormsApp9/WinFormsApp9/Form1.cs
@@ -0,0 +1,19 @@
+namespace WinFormsApp9
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			String adsoyad,meslek
+				adsoyad = "Büþra KARA"
+				meslek = "Öðrenci"
+				Label1.Text = adsoyad
+				Label2.Text = meslek
+		}
+	}
+}

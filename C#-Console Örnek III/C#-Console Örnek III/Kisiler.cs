c91a0586483e436c53dfff593a3d95a22c2e8af9
busrax1 <bkara3406@gmail.com>
2023-03-10 17:00:24 +0300

    Örne.Console III
    Project has been created.

---
+++ C#-Console Örnek III/C#-Console Örnek III/Kisiler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Örnek_III
+{
+	//Klavyeden girmiş olduğumuz metni ekrana 10 defa yazdır.
+	internal class Kisiler
+	{
+		public void kisilistesi(string adsoyad)
+		{
+			for (int i=0; i<=10; i++)
+			{
+				Console.WriteLine(adsoyad);			}
+		}
+	}
+}
---
+++ C#-Console Örnek III/C#-Console Örnek III/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Örnek_III
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Kisiler ks = new Kisiler();
+			ks.kisilistesi("Büşra KARA");
+			ks.kisilistesi("Ramazan KARA");
+			Console.Read();
+		}
+	}
+}

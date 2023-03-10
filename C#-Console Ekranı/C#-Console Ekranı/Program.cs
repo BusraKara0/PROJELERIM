69845199e702a9ae9c6612494c8b3767ad80c62e
busrax1 <bkara3406@gmail.com>
2023-03-10 16:45:35 +0300

    Örnek.Console
    Project has been created.

---
+++ C#-Console Ekranı/C#-Console Ekranı/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Ekranı
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Merhaba");
+			Console.WriteLine("Dünyam");
+
+			string kelime;
+			kelime = "26 Ekim";
+			Console.WriteLine(kelime);
+
+			int sayi = 5;
+			Console.WriteLine(sayi);
+
+			Console.Read();
+		}
+	}
+}

e7c9c59c6372337197af18ed08821e9a81acee32
busrax1 <bkara3406@gmail.com>
2023-03-10 16:58:38 +0300

    Örnek.Console II
    Project has been created.

---
+++ C#-Console Örnek II/C#-Console Örnek II/Kisiler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Örnek_II
+{
+	internal class Kisiler
+	{
+		public void kisilistesi()
+		{
+			for (int i=0; i<=10; i++)
+			{
+				Console.WriteLine(i + ".Kişi");
+			}
+		}
+	}
+}
---
+++ C#-Console Örnek II/C#-Console Örnek II/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Örnek_II
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Kisiler ks = new Kisiler();
+			ks.kisilistesi();
+			Console.Read();
+		}
+	}
+}

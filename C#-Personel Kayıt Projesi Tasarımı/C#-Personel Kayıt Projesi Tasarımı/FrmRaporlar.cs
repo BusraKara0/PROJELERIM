16e05ef6b7d1639926669fc8b31055ae32aba34d
busrax1 <bkara3406@gmail.com>
2023-03-10 16:10:46 +0300

    Örnek.Personel Kayıt
    Proje has been created.

---
+++ C#-Personel Kayıt Projesi Tasarımı/C#-Personel Kayıt Projesi Tasarımı/FrmRaporlar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__Personel_Kayıt_Projesi_Tasarımı
+{
+	public partial class FrmRaporlar : Form
+	{
+		public FrmRaporlar()
+		{
+			InitializeComponent();
+		}
+
+		private void FrmRaporlar_Load(object sender, EventArgs e)
+		{
+
+            this.reportViewer1.RefreshReport();
+            this.reportViewer1.RefreshReport();
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}

c800226387d72c523f466fe2ae2b8fad146f0adb
busrax1 <bkara3406@gmail.com>
2023-03-10 20:59:14 +0300

    Örnek.Form8
    Project has been created.

---
+++ WinFormsApp8/WinFormsApp8/Form1.cs
@@ -0,0 +1,17 @@
+namespace WinFormsApp8
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string metin
+				metin = "merhaba bugün 24 þubat 2023 cuma"
+				label1.Text = metin
+		}
+	}
+}

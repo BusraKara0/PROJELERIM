6e02e3ef40ecfc841cb0b2911ce7498ff2495a5f
busrax1 <bkara3406@gmail.com>
2023-03-10 20:20:46 +0300

    Örnek.Veri Taşıma Ödev
    Project has been created.

---
+++ C#-Veri Taşıma Ödev/C#-Veri Taşıma Ödev/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__Veri_Taşıma_Ödev
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Form2 fm = new Form2();
+			fm.ad = textBox1.Text;
+			fm.soyad = textBox2.Text;
+			fm.meslek = textBox3.Text;
+			fm.Show();
+			this.Hide();
+		}
+	}
+}
---
+++ C#-Veri Taşıma Ödev/C#-Veri Taşıma Ödev/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__Veri_Taşıma_Ödev
+{
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			InitializeComponent();
+		}
+		public string ad;
+		public string soyad;
+		public string meslek;
+		private void Form2_Load(object sender, EventArgs e)
+		{
+			comboBox1.Text = ad;
+			comboBox2.Text = soyad;
+			comboBox3.Text = meslek;
+		}
+	}
+}

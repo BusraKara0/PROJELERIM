c1992a89d7e0d13fba5ddba9149bb3b0fa049d82
busrax1 <bkara3406@gmail.com>
2023-03-10 16:01:17 +0300

    Örnek.Uçak Bilet Rezervasyonu
    Project has been created

---
+++ Uçak Bilet Rezervasyon Sistemi/Uçak Bilet Rezervasyon Sistemi/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add("Rota:" + ComboBox1.Text + " - " + ComboBox2.Text + "Tarih:" + DateTimePicker1.Text + "Saat:" + MaskedTextBox1.Text + "Yolcu Bilgileri~Ad-Soyad:" + TextBox1.Text + "TC:" + MaskedTextBox2.Text + "Telefon:" + MaskedTextBox3.Text)
+        MessageBox.Show("Yolcu Kaydı Yapıldı")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label9.Text = ComboBox2.Text
+        ComboBox2.Text = ComboBox1.Text
+        ComboBox1.Text = Label9.Text
+    End Sub
+End Class

a9781895a444f45f310ffc5e8518339ee23f0bde
busrax1 <bkara3406@gmail.com>
2023-03-10 21:02:34 +0300

    Örnek.Form11
    Project has been created.

---
+++ WinFormsApp11/WinFormsApp11/Form1.cs
@@ -0,0 +1,17 @@
+namespace WinFormsApp11
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			String metin
+				metin = "merhabaa"
+				Label1.Text = metin;
+		}
+	}
+}

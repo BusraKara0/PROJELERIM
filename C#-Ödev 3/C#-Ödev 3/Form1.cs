c0608e1f64e44fab64d79d324c091c3cc4877915
busrax1 <bkara3406@gmail.com>
2023-03-10 19:52:08 +0300

    Örnek.Ödev III
    Project has been created.

---
+++ C#-Ödev 3/C#-Ödev 3/Form1.cs
@@ -0,0 +1,21 @@
+namespace C__Ödev_3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string ürünadı;
+			int fiyat, indirim1, indirim2;
+			ürünadı = label1.Text;
+			fiyat =
+			indirim1 = Convert.ToInt16(label2.Text);
+			indirim2 = Convert.ToInt16(label3.Text);
+			indirim1 = ()
+		}
+	}
+}
---
+++ C#-Ödev 3/C#-Ödev 3/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__Ödev_3
+{
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string urunadı;
+			int fiyat, indirim, sonuc;
+			urunadı = textBox1.Text;
+			fiyat = Convert.ToInt16(textBox2.Text);
+			indirim = Convert.ToInt16(textBox3.Text);
+			sonuc = (fiyat * 8) / 100;
+			listBox1.Items.Add(urunadı + " " + fiyat + "KDV:" + sonuc);
+		}
+	}
+}

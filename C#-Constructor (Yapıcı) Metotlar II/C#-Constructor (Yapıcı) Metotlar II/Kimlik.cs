4553d07d99c6bc978942b905b289b9a5ea4eb1f0
busrax1 <bkara3406@gmail.com>
2023-03-10 17:05:51 +0300

    Örnek.Yapıcı Metotlar I
    Project has been created.

---
+++ C#-Constructor (Yapıcı) Metotlar II/C#-Constructor (Yapıcı) Metotlar II/Kimlik.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Constructor__Yapıcı__Metotlar_II
+{
+	internal class Kimlik
+	{
+		string ad;
+		string soyad;
+		string memleket;
+		int yas;
+		char cinsiyet;
+
+		public string AD
+		{
+			get { return ad; }
+			set { ad = value.ToLower(); }
+		}
+		public string SOYAD
+		{
+			get { return soyad; }
+			set { soyad = value.ToUpper(); }
+		}
+		public string MEMLEKET
+		{
+			get { return memleket; }
+			set { memleket = value; }
+		}
+		public int YAS
+		{
+			get { return yas; }
+			set { yas = Math.Abs(value); }
+		}
+		public char CINSIYET
+		{
+			get { return cinsiyet; }
+			set { cinsiyet = value; }
+		}
+		public Kimlik()
+		{
+			ad = " ";
+			soyad = " ";
+			memleket = "Ankara";
+			yas = 18;
+			cinsiyet = 'K';
+		}
+	}
+}
---
+++ C#-Constructor (Yapıcı) Metotlar II/C#-Constructor (Yapıcı) Metotlar II/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Constructor__Yapıcı__Metotlar_II
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Kimlik kml = new Kimlik();
+			kml.AD = "Büşra";
+			kml.SOYAD = "KARA";
+			kml.MEMLEKET = "Adana";
+			Console.WriteLine(kml.AD);
+			Console.WriteLine(kml.SOYAD);
+			Console.WriteLine(kml.MEMLEKET);
+			Console.WriteLine(kml.YAS);
+			Console.WriteLine(kml.CINSIYET);
+			Console.Read();
+		}
+	}
+}

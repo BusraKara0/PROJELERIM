4ebc7956696e12d9f06bec32064bf6aa4034388c
busrax1 <bkara3406@gmail.com>
2023-03-10 20:54:10 +0300

    Örnek.Form3
    Project has been created.

---
+++ WinFormsApp3/WinFormsApp3/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(MaskedTextBox1.Text + " " + MaskedTextBox2.Text + " " + MaskedTextBox3.Text + " " + MaskedTextBox4.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Add(DateTimePicker1.Text)
+    End Sub
+End Class

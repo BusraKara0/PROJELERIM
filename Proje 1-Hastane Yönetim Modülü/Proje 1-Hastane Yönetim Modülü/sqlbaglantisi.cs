a70c4655f20846307892f67639afc44676eb7e95
busrax1 <bkara3406@gmail.com>
2023-03-10 15:00:17 +0300

    Proje.Hastane
    Project has been created.

---
+++ Proje 1-Hastane Yönetim Modülü/Proje 1-Hastane Yönetim Modülü/sqlbaglantisi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Proje_1_Hastane_Yönetim_Modülü
+{
+	internal class sqlbaglantisi
+	{
+		public SqlConnection baglanti()
+		{
+			SqlConnection baglan = new SqlConnection("Data Source=DESKTOP-BMF26BV;Initial Catalog=HastaneProje;Integrated Security=True");
+			baglan.Open();
+			return baglan;
+		}
+	}
+}

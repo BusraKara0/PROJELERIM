5034da67dc04bcbd84295bfa19205f347b3c2cdc
busrax1 <bkara3406@gmail.com>
2023-03-10 20:58:03 +0300

    Örnek.Form7
    Project has been created.

---
+++ WinFormsApp7/WinFormsApp7/Form1.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Reflection
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        String metin
+         metin = "Merhaba Bugün 24 Şubat 2023 Cuma"
+        Label1.Text = metin
+    End Sub
+End Class

aa930b7f35ce52fb1c3786cec5222b5186beeaac
busrax1 <bkara3406@gmail.com>
2023-03-10 16:54:21 +0300

    Örnek.Console Ödev III
    Project has been created.

---
+++ C#-Console Ödev III/C#-Console Ödev III/Kisi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Ödev_III
+{
+	internal class Kisi
+	{
+		public Kisi(string bilgi)
+		{
+			Console.WriteLine("Durum:" + bilgi);
+		}
+	}
+}
---
+++ C#-Console Ödev III/C#-Console Ödev III/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Console_Ödev_III
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string blg;
+			Console.Write("Ad - Soyad - Yaş - Meslek");
+			blg = Console.ReadLine();
+			Kisi ks = new Kisi("Büşra - KARA - 19 - Öğrenci");
+			Console.Read();
+		}
+	}
+}

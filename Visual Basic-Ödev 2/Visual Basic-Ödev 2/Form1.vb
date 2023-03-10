bdc1c1ea292f8f89a20a70ca1248e7fbfdd5db14
busrax1 <bkara3406@gmail.com>
2023-03-10 20:44:25 +0300

    Örnek.Ödevs II
    Project has been created.

---
+++ Visual Basic-Ödev 2/Visual Basic-Ödev 2/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(TextBox1.Text + " " + TextBox2.Text + " " + MaskedTextBox1.Text + " " + MaskedTextBox2.Text + " " + ComboBox1.Text + " " + DateTimePicker1.Text)
+    End Sub
+End Class

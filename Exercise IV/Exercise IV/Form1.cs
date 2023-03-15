0a1ed4932188ee0e80c20fc0d2e424bca0373681
busrax1 <bkara3406@gmail.com>
2023-03-15 14:58:00 +0300

    Örnek.Exercise IV
    Project has been created.

---
+++ Exercise IV/Exercise IV/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_IV
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		int s, x;
+		private void button1_Click(object sender, EventArgs e)
+		{
+			s = Convert.ToInt16(textBox1.Text);
+			x = (s * 18) / 100;
+			label2.Text = x.ToString();
+		}
+	}
+}

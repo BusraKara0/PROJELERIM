88952c9b1ef0cc14f8a2e1fa9238981cc23ef096
busrax1 <bkara3406@gmail.com>
2023-03-15 15:00:22 +0300

    Örnek.Exercise VI
    Project has been created.

---
+++ Exercise VI/Exercise VI/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_VI
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		int b;
+		private void button1_Click(object sender, EventArgs e)
+		{
+			b = Convert.ToInt16(textBox1.Text);
+			if(b % 4 == 0 && b % 7 == 0)
+			{
+				textBox2.Text = "Bu sayı 7 ve 4'ün katı";
+			}
+			else
+			{
+				textBox2.Text = "Başka bir sayı girin.";
+			}
+		}
+	}
+}
